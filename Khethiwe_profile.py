df8a451e300dfabf84d0e7a62ba83c12060a712d
Khethiwe Ntshangase <khethiwentshangase22@gmail.com>
2026-02-01 19:24:07 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- Khethiwe_profile.py
+++ Khethiwe_profile.py
@@ -115,7 +115,7 @@ elif menu == "About":
     Mathematical Sciences graduate with a strong foundation in statistics, data science, programming, and quantitative analysis, 
     and hands-on experience working with real-world datasets at Statistics South Africa and CPUT. Skilled in data analysis, reporting, 
     R, SQL, SAS, Python, and Power BI, with experience in data validation, trend analysis, and producing analytical reports.
-    Highly interested in credit risk, decision science, and analytics, and eager to apply quantitative skills in a structured risk environment
+    Highly interested in credit risk, decision science, and analytics, and eager to apply quantitative skills in a structured risk environment.
     </div>
     """, unsafe_allow_html=True)
 
@@ -225,6 +225,7 @@ elif menu == "Contact":
     <h4>📧 khethiwentshangase22@gmail.com</h4>
     </div>
     """, unsafe_allow_html=True)
+
 
 
 
